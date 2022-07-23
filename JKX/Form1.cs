864f0e1e711573a1d410c8f4d36387cdfff325df
ZvalsoraY <yazamail@yandex.ru>
2022-07-23 21:17:23 +0300

    qwe

--- JKX/Form1.cs
+++ JKX/Form1.cs
@@ -1,9 +1,19 @@
+using Interfaces;
+
 namespace JKX
 {
     public partial class Form1 : Form
     {
-        public Form1()
+        private ICounterBL _counterBL;
+        private IRateBL _rateBL;
+        private IReadingBL _readingBL;
+        private IServiceBL _serviceBL;
+        public Form1(ICounterBL counterBL, IRateBL rateBL, IReadingBL readingBL, IServiceBL serviceBL)
         {
+            _counterBL = counterBL;
+            _rateBL = rateBL;
+            _readingBL = readingBL;
+            _serviceBL = serviceBL;
             InitializeComponent();
         }
     }
