8c0d2454e6b7f27d776557f4a134437a21537f6a
ZvalsoraY <yazamail@yandex.ru>
2022-08-01 00:04:59 +0300

    prepearing debt

--- WebJkx/Controllers/ServiceReadingsController.cs
+++ WebJkx/Controllers/ServiceReadingsController.cs
@@ -7,12 +7,14 @@ namespace WebJkx.Controllers
     {
         private readonly IServiceDAO serviceDAO;
         private readonly IReadingDAO readingDAO;
+        private readonly IRateDAO rateDAO;
 
-        
-        public ServiceReadingsController(IServiceDAO serviceDAO, IReadingDAO readingDAO)
+
+        public ServiceReadingsController(IServiceDAO serviceDAO, IReadingDAO readingDAO, IRateDAO rateDAO)
         {
             this.serviceDAO = serviceDAO;
             this.readingDAO = readingDAO;
+            this.rateDAO = rateDAO;
         }
 
         [HttpGet]
@@ -82,5 +84,46 @@ namespace WebJkx.Controllers
             return View();
         }
 
+        [HttpGet]
+        public IActionResult ShowDebt(int id)
+        {
+            var service = serviceDAO.GetServiceById(id);
+
+            var readingsOfService = readingDAO.GetServicesReadings(service);
+
+            var ratesOfService = rateDAO.GetServicesRates(service);
+            double sumDept = 0;
+
+            if (readingsOfService.Count > 0 && ratesOfService.Count > 0)
+            {
+                ratesOfService.OrderBy(d => d.EndData);
+                for(int i = 0; i < ratesOfService.Count; i++)
+                {
+                    double readingIni = ratesOfService[i].ini;
+                    double readingLast = 0;
+                    int lastIndex = 0;
+                    DateTime datePerem = DateTime.Now.AddYears(-150);
+                    foreach(var r in readingsOfService)
+                    {
+                        if (r.TransDate < ratesOfService[i].EndData && r.TransDate > datePerem)
+                        {
+                            datePerem = r.TransDate;
+                            readingLast = r.CurValue;
+                        }
+                    }
+                    sumDept += (readingLast - readingIni) * ratesOfService[i].Price 
+                }
+                return View(readingsOfService);
+            }
+
+
+
+            //if (readingsOfService.Count > 0)
+            //{
+            //    return View(readingsOfService);
+            //}
+
+            return RedirectToAction("DebtError");
+        }
     }
 }
